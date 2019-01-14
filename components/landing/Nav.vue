006d5a3cfa2f37c6769b9afb2f22ee08abeb89bc
elegant651 <korea651@gmail.com>
2019-01-14 11:32:01 +0900

    add link - developers

--- components/landing/Nav.vue
+++ components/landing/Nav.vue
@@ -12,6 +12,9 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">          
           <ul class="navbar-nav ml-auto">
             <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="http://api.avarkey.com/api">Developers</a>
+            </li>
+            <li class="nav-item mx-0 mx-lg-1">
               <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" v-on:click="goMenu('generate')" href="#generate">Generate</a>
             </li>
             <li class="nav-item mx-0 mx-lg-1">
